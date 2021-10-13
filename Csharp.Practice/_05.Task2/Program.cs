d2fcd7292d2891224dd7f24c381c17f2bd071317
Anastasiya <anastasiaf0311@yandex.by>
2021-10-13 20:59:27 +0300

    add task2

---
+++ Csharp.Practice/_05.Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short shortValue = 1;
+
+            object obj = shortValue;
+
+            sbyte sbyteValue = Convert.ToSByte(obj);
+
+            Console.WriteLine($"Variable value = {sbyteValue}");
+            Console.WriteLine($"Type is {sbyteValue.GetType()}");
+        }
+    }
+}
