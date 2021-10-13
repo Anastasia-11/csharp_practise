3ec4b9d1ea3de9cea9cdba9765ff4926d1fcfb87
Anastasiya <anastasiaf0311@yandex.by>
2021-10-13 20:45:58 +0300

    add task1

--- Csharp.Practice/_05.Task1/Program.cs
+++ Csharp.Practice/_05.Task1/Program.cs
@@ -6,7 +6,14 @@ namespace _05.Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = DateTime.Now.TimeOfDay.Hours switch
+            {
+                var hour when (hour > 9 && hour < 12) => "Good morning, guys",
+                var hour when (hour > 12 && hour < 15) => "Good day, guys",
+                var hour when (hour > 15 && hour < 22) => "Good evening, guys",
+                _ => "It's night"
+            };
+            Console.WriteLine(message);
         }
     }
 }
