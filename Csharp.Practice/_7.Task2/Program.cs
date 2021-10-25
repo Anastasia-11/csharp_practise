90a390d99a40a7d7ac746f04d91230d56b8f47c3
Anastasiya <anastasiaf0311@yandex.by>
2021-10-25 20:39:26 +0300

    add task2

---
+++ Csharp.Practice/_7.Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] arr = new object[] {32L, 'A', "Hey"};
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+                Console.Write(' ');
+            }
+
+            arr[0] = (long)arr[0] + int.MaxValue;
+            arr[2] = arr[2] + " guys!";
+            
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+                Console.Write(' ');
+            }
+        }
+    }
+}
