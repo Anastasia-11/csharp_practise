82820a12bd9ece88eb025931843413669d07e8a4
Anastasiya Fedorovich <anastasiaf0311@yandex.by>
2021-12-01 12:56:08 +0300

    add lecture_16 practise

---
+++ Csharp.Practice/_16.Task1/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace _16.Task1.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Modify(this string str)
+        {
+            return str.Substring(0, 5) + "...";
+        }
+    }
+}
---
+++ Csharp.Practice/_16.Task1/Models/UniqueItem.cs
@@ -0,0 +1,20 @@
+﻿namespace _16.Task1.Models
+{
+    public class UniqueItem
+    {
+        private static int Id = default;
+        public int ItemId { get; }
+        
+        public UniqueItem()
+        {
+            Id++;
+            ItemId = Id;
+        }
+        
+        public UniqueItem(int id)
+        {
+            Id = id;
+            ItemId = Id;
+        }
+    }
+}
